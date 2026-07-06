72f6a1296b47cd4e6d3cd3aba534a4d29fdfa850
kokone164 <degawakokone@outlook.jp>
2026-07-06 10:30:01 +0900

    演習１

---
+++ 20260706/20260706/20260706_Function1_degawakokone.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include"20260706_Header1_degawakokone.h"
+
+//20‰ñ•œ
+void Heal(int& hpNum)
+{
+	hpNum += HEALNUM;
+}
---
+++ 20260706/20260706/20260706_PracMain1_degawakokone.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include"20260706_Header1_degawakokone.h"
+using namespace std;
+
+int main(void)
+{
+	int hp = 100;
+	int num = 0;
+
+	cout << "HPを回復しますか？\n現在のHP:" << hp << "\nYes：１No：２" << endl;
+
+	cin >> num;
+
+	if (num == 1)
+	{
+		Heal(hp);
+		cout << "HPを回復しました。\n現在のHP:" << hp << endl;
+	}
+	else
+	{
+		cout << "HPの回復はありません。\n現在のHP:" << hp << endl;
+	}
+	return 0;
+}
