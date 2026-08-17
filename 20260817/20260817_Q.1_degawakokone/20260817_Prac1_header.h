5e68d5f9d6525abe2c8ccd0aefd7cbde9aa170fb
Roll <degawakokone@outlook.jp>
2026-08-17 11:04:48 +0900

    問１終了

---
+++ 20260817/20260817_Q.1_degawakokone/20260817_Prac1_header.h
@@ -0,0 +1,13 @@
+#pragma once
+/// <summary>
+/// ピッチングタイプ
+/// </summary>
+/// <param name="piting">
+/// プレイヤーが投げた球
+/// </param>
+void PitingType(int piting);
+/// <summary>
+/// 勝敗判定
+/// </summary>
+/// <param name="out">アウト数</param>
+void Result(int out);
